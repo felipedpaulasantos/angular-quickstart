ba66adfa24b704313c70e4c93a85ff18b8940950
Felipe <felipedpaulasantos@gmail.com>
2023-04-13 20:48:08 -0300

    Custom-style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,46 +2,54 @@ import { Component, OnInit } from "@angular/core";
 import { UntypedFormBuilder, Validators } from "@angular/forms";
 import { AccordionMenu } from "../shared/components/accordion/types/accordion-menu";
 import { mockedSideMenuItems } from "../shared/constants";
-
+import { CardPanelStyle } from "../guia-caixa/components/card-panel/card-panel-style.enum";
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+	selector: "app-home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
 
-  constructor(
-    private fb: UntypedFormBuilder  ) {}
+	constructor(private fb: UntypedFormBuilder) {}
 
-  rows: any[] = [];
-  resources: AccordionMenu[] = mockedSideMenuItems;
+	cardPanelStyle: CardPanelStyle = CardPanelStyle.HORIZONTAL;
 
-  contratos = [];
+	rows: any[] = [];
+	resources: AccordionMenu[] = mockedSideMenuItems;
 
-  formCpfNis = this.fb.group({
-    cpf: [null, [Validators.required]],
-    nis: [null, [Validators.required]]
-  });
-  cliente = null;
+	contratos = [];
 
-  previaSrc: string;
-  uploadedFile: File = null;
+	formCpfNis = this.fb.group({
+		cpf: [null, [Validators.required]],
+		nis: [null, [Validators.required]],
+	});
+	cliente = null;
 
-  ngOnInit() {
-    this.rows = this.groupColumns(this.resources);
-  }
+	previaSrc: string;
+	uploadedFile: File = null;
 
-  groupColumns(resources: any[]): any[] {
-    const filteredResources = resources.filter(resource => {
-      return (resource.enabled && resource.isLink) &&
-      (resource.name != "Início");
-    });
-    const newRows = [];
-    for (let index = 0; index < filteredResources.length; index += 3) {
-      newRows.push(filteredResources.slice(index, index + 3));
-    }
+	ngOnInit() {
+		this.rows = this.groupColumns(this.resources);
+	}
 
-    return newRows;
-  }
+	groupColumns(resources: any[]): any[] {
+		const filteredResources = resources.filter((resource) => {
+			return (
+				resource.enabled &&
+				resource.isLink &&
+				resource.name != "Início"
+			);
+		});
+		const newRows = [];
+		for (
+			let index = 0;
+			index < filteredResources.length;
+			index += 3
+		) {
+			newRows.push(filteredResources.slice(index, index + 3));
+		}
+
+		return newRows;
+	}
 }
